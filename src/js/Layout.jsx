fb3e65db72c9f7280624ffee3e9228b9dd3773a5
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-08-06 01:29:39 +0000

    change layout

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Provider } from "./stores/AppContext.jsx";
-
 import ScrollToTop from "./component/ScrollToTop.jsx";
 import Home from "./views/Home.jsx";
 import Profile from "./views/Profile.jsx";
